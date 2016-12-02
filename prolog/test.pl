603dcbef2bc36a181ce02f5b2b5a2214cfc45c91
Carl Olson <carl.anders.olson@gmail.com>
2016-12-02 16:47:15 -0600

    update prolog

--- prolog/test.pl
+++ prolog/test.pl
@@ -1,16 +1,16 @@
 
-:- ['easy_test/easy_test'].
+user:file_search_path(git, Path) :-
+    expand_file_name('~/git/', [Root]),
+    directory_files(Root, GitDirs),
+    member(Dir, GitDirs),
+    string_concat(Root, Dir, Path).
 
 :- include('main.pl').
 
-:- initialization main.
+:- use_module(git(easy_test)).
 
 test :-
-    describe(test/0,
+    describe("",
 	     [
 		 ""
 	     ]).
-
-main :-
-    test, halt;
-    halt.
