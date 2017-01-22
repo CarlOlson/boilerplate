5424af7c0b4856df72f086eae40d4cb74c605a63
Carl Olson <carl.anders.olson@gmail.com>
2017-01-22 14:47:47 -0600

    add non-webapp js project boilerplate

---
+++ js/stryker.conf.js
@@ -0,0 +1,17 @@
+module.exports = function(config){
+  config.set({
+    files: [
+      {
+	pattern: 'src/**/*.js',
+	mutated: true,
+	included: false
+      },
+      'test/**/*.js'
+    ],
+    testRunner: 'mocha',
+    testFramework: 'mocha',
+    coverageAnalysis: 'perTest',
+    reporter: ['clear-text',
+	       'progress'],
+  });
+}
---
+++ js/test/main.test.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+const main = require('../src/main');
+
+describe('', () => {
+  it('', () => {
+  });
+});
