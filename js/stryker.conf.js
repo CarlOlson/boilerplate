53671be88b6ed349d144f78ff8b61e8ef5b750d9
Carl Olson <carlolson@users.noreply.github.com>
2017-03-21 10:15:17 -0500

    update js indentation

--- js/stryker.conf.js
+++ js/stryker.conf.js
@@ -1,21 +1,21 @@
-module.exports = function(config){
-  config.set({
-    files: [
-      {
-	pattern: 'src/**/*.js',
-	mutated: true,
-	included: false
-      },
-      'test/**/*.js'
-    ],
-    testRunner: 'mocha',
-    testFramework: 'mocha',
-    coverageAnalysis: 'all',
-    reporter: ['clear-text',
-	       'progress'],
-    clearTextReporter: {
-      maxTestsToLog: 1
-    },
-    logLevel: 'warn'
-  });
+module.exports = function(config) {
+    config.set({
+	files: [
+	    {
+		pattern: 'src/**/*.js',
+		mutated: true,
+		included: false
+	    },
+	    'test/**/*.js'
+	],
+	testRunner: 'mocha',
+	testFramework: 'mocha',
+	coverageAnalysis: 'all',
+	reporter: ['clear-text',
+		   'progress'],
+	clearTextReporter: {
+	    maxTestsToLog: 1
+	},
+	logLevel: 'warn'
+    });
 }
--- js/test/main.test.js
+++ js/test/main.test.js
@@ -2,6 +2,6 @@ const expect = require('chai').expect;
 const main = require('../src/main');
 
 describe('', () => {
-  it('', () => {
-  });
+    it('', () => {
+    });
 });
